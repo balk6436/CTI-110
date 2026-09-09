6f66d0f80793f8571ddb7a4eec027c3b295e32d6
balk6436 <balk6436@student.faytechcc.edu>
2026-09-09 11:21:51 -0400

    Add files via upload

---
+++ P1LAB1_BalKainoa.py
@@ -0,0 +1,13 @@
+# Kainoa Bal
+# 09/09/2026
+# P1LAB1
+# Inputs and outputs
+
+#Input for first name
+first_name = input ("What is your first name: ")
+
+#Input for last name
+last_name = input ("What is your last name: ")
+
+#Output message to user
+print ("Hello", first_name, last_name + "!", "Welcome to CTI-110")
